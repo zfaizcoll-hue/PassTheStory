cd27e2d60c5ee08f5a7d3a541751e3f3ac3d3225
zfaizcoll-hue <zfaizcoll@gmail.com>
2026-09-20 20:03:45 -0400

    Add first story contribution

--- PassTheStory.java
+++ PassTheStory.java
@@ -4,6 +4,7 @@ public class PassTheStory {
         String[] story = new String[10];
 
         story[0] = "Once upon a time, a kid discovered a mysterious door in the forest, and then.";
+        story[1] = "The kid opened the door and found a raccoon wearing sunglasses holding a golden potato, and then.";
 
         for (String sentence : story) {
             if (sentence != null) {
